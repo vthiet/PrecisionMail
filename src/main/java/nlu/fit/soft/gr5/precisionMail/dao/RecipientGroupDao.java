3da01c30d9d326cf0bef2b55ab96ae1b764dfa9b
Pluf_nluer <23130240@st.hcmuaf.edu.vn>
2026-06-04 21:11:07 +0700

    feat(uc-02): thêm RecipientGroupDao interface - contract lưu trữ

--- src/main/java/nlu/fit/soft/gr5/precisionMail/dao/RecipientGroupDao.java
+++ src/main/java/nlu/fit/soft/gr5/precisionMail/dao/RecipientGroupDao.java
@@ -1,4 +1,47 @@
 package nlu.fit.soft.gr5.precisionMail.dao;
 
-public class RecipientGroupDao {
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import nlu.fit.soft.gr5.precisionMail.model.RecipientGroup;
+
+/**
+ * DAO Interface for RecipientGroup operations
+ */
+public interface RecipientGroupDao {
+    /**
+     * Save new recipient group
+     */
+    RecipientGroup save(RecipientGroup group) throws IOException;
+
+    /**
+     * Update existing recipient group
+     */
+    RecipientGroup update(RecipientGroup group) throws IOException;
+
+    /**
+     * Find recipient group by ID
+     */
+    Optional<RecipientGroup> findById(Long id) throws IOException;
+
+    /**
+     * Find recipient group by name
+     */
+    Optional<RecipientGroup> findByName(String name) throws IOException;
+
+    /**
+     * Get all recipient groups
+     */
+    List<RecipientGroup> findAll() throws IOException;
+
+    /**
+     * Delete recipient group by ID
+     */
+    void deleteById(Long id) throws IOException;
+
+    /**
+     * Delete recipient group by name
+     */
+    void deleteByName(String name) throws IOException;
 }

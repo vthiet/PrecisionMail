eea76c05da248ab1ce463fd58852df7f21ccb70c
ducphat0311@gmail.com <ducphat0311@gmail.com>
2026-06-05 12:47:04 +0700

    feat(uc-04): thêm chức năng export hàng đợi ra file excel

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,8 @@ module precisionMail {
     requires com.fasterxml.jackson.datatype.jsr310;
     requires org.slf4j;
     requires ch.qos.logback.classic;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
     opens nlu.fit.soft.gr5.precisionMail to javafx.fxml;
     opens nlu.fit.soft.gr5.precisionMail.controller to javafx.fxml;
